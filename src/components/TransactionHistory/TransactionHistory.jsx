4baa773fd3a8b7174c82520fd81cc3d5fcad5f59
vladostapovich <jorabumer@ukr.net>
2024-05-12 16:48:19 +0300

    updatacsstransaction

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -4,20 +4,20 @@ function TransactionHistory({ items }) {
   return (
     <table>
       <thead>
-        <tr className="topic-bank">
-          <th className="text-topic-bank">Type</th>
-          <th className="text-topic-bank">Amount</th>
-          <th className="text-topic-bank">Currency</th>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map((item) => {
           return (
-            <tr key={item.id} className="raw-info">
-              <td className="text-bank">{item.type}</td>
-              <td className="text-bank">{item.amount}</td>
-              <td className="text-bank">{item.currency}</td>
+            <tr key={item.id} className="info">
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
             </tr>
           );
         })}
